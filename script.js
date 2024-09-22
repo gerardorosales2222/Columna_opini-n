16eac5d4a5b124f88e4e48f5d03431995011871b
gerardorosales2222 <gerardorosales2222@gmail.com>
2024-09-22 11:39:00 -0300

    add navbar flotante

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let tamanioActual = 1; // Tamaño inicial (1)
-const tamanos = ["20px", "30px", "40px", "50px", "60px","70px"]; // Array de tamaños es
+let tamanioActual = 1; 
+const tamanos = ["20px", "30px", "40px", "50px", "60px","70px"];
 function disminuirTamano() {
       if (tamanioActual > 0) {
           tamanioActual--;
@@ -15,7 +15,7 @@ function aumentarTamano() {
 }
 
 function cambiarTamano() {
-      let parrafos = document.getElementsByTagName("p");
+      let parrafos = document.getElementsByClassName("texto");
       let h1 = document.getElementsByTagName("h1");
       for (let i = 0; i < parrafos.length; i++) {
           parrafos[i].style.fontSize = tamanos[tamanioActual];
