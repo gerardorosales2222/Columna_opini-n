da4dd80c3b3fab3dbdeadaa94403af10b66f0b75
gerardorosales2222 <gerardorosales2222@gmail.com>
2024-09-22 14:27:48 -0300

    Fix tamaño de todos los textos

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let tamanioActual = 1; 
-const tamanos = ["20px", "30px", "40px", "50px", "60px","70px"];
+const tamanos = ["20px", "25px", "30px", "45px", "60px","70px"];
+const titulo = ["40px", "50px", "60px","65px","70px","80px"];
 function disminuirTamano() {
       if (tamanioActual > 0) {
           tamanioActual--;
@@ -19,6 +20,13 @@ function cambiarTamano() {
       let h1 = document.getElementsByTagName("h1");
       for (let i = 0; i < parrafos.length; i++) {
           parrafos[i].style.fontSize = tamanos[tamanioActual];
-          h1[i].style.fontSize = tamanos[tamanioActual+1];
+          h1[i].style.fontSize = titulo[tamanioActual];
       }
  }
+
+const btn_disminuir = document.getElementById("disminuir");
+btn_disminuir.addEventListener("click", disminuirTamano);
+
+const btn_aumentar = document.getElementById("aumentar");
+btn_aumentar.addEventListener("click", aumentarTamano);
+
